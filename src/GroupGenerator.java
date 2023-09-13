173eaa78f62e0f28f5cfa98db122b24d5311a526
Rimas Alghamdi <reemas.y24@gmail.com>
2023-09-13 12:17:44 +0300

    First Edited

--- src/GroupGenerator.java
+++ src/GroupGenerator.java
@@ -10,19 +10,20 @@ public class GroupGenerator {
             System.out.print("Enter the number of groups to create: ");
             int numGroups = scanner.nextInt();
 
-            List<String> studentNames = readStudentNamesFromFile("C:\\Users\\Rimas Alghamdi\\eclipse-workspace\\lab1-SWE206\\src\\Names.txt");
+            List<String> studentNames = readStudentNamesFromFile(
+                    "C:\\Users\\Rimas Alghamdi\\eclipse-workspace\\lab1-SWE206\\src\\Names.txt");
 
             List<List<String>> groups = generateGroupsByNum(studentNames, numGroups);
-
 
             for (int i = 0; i < groups.size(); i++) {
                 System.out.println("Group " + (i + 1) + ": " + groups.get(i));
             }
-        }else if (way == 2) {
+        } else if (way == 2) {
             System.out.print("Enter the maximum number of students in each group: ");
             int maxStudentsInGroup = scanner.nextInt();
 
-            List<String> studentNames = readStudentNamesFromFile("C:\\Users\\Rimas Alghamdi\\eclipse-workspace\\lab1-SWE206\\src\\Names.txt");
+            List<String> studentNames = readStudentNamesFromFile(
+                    "C:\\Users\\Rimas Alghamdi\\eclipse-workspace\\lab1-SWE206\\src\\Names.txt");
 
             List<List<String>> groups = generateGroupsByMax(studentNames, maxStudentsInGroup);
 
@@ -31,26 +32,6 @@ public class GroupGenerator {
             }
         }
 
-
-    }
-    private static List<String> readStudentNamesFromFile(String fileName) {
-        List<String> studentNames = new ArrayList<>();
-
-        try {
-            File file = new File(fileName);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String name = scanner.nextLine().trim();
-                if (!name.isEmpty()) {
-                    studentNames.add(name);
-                }
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + fileName);
-        }
-
-        return studentNames;
     }
 
     // Generates random groups from the list of student names
@@ -84,23 +65,4 @@ public class GroupGenerator {
         return groups;
     }
 
-    private static List<List<String>> generateGroupsByMax(List<String> studentNames, int maxStudentsInGroup) {
-        List<List<String>> groups = new ArrayList<>();
-
-        // Shuffle the student names randomly
-        Collections.shuffle(studentNames);
-
-        int numStudents = studentNames.size();
-        int numGroups = (int) Math.ceil((double) numStudents / maxStudentsInGroup);
-
-        int currentIndex = 0;
-        for (int i = 0; i < numGroups; i++) {
-            int startIndex = i * maxStudentsInGroup;
-            int endIndex = Math.min(startIndex + maxStudentsInGroup, numStudents);
-            List<String> group = studentNames.subList(startIndex, endIndex);
-            groups.add(group);
-        }
-
-        return groups;
-    }
 }
